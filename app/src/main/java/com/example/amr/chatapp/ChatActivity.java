b1779e9feaf87250a7fb0c0987876e2965259055
Amr <amribrahimgazala@gmail.com>
2019-08-19 03:21:16 +0200

    Display list of Chats by using RecycleView firebaseAdapter

--- app/src/main/java/com/example/amr/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/amr/chatapp/ChatActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private String messageReceiverID , messageReceiverName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
+        messageReceiverName = getIntent().getExtras().get("visit_user_name").toString();
     }
 }
